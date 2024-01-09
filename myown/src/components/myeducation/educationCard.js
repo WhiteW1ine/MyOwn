23ebfb9c29bd1061309ea0f432e90539b6f9d79b
WhiteW1ine <foxratmir2000@gmail.com>
2024-01-09 18:47:09 +0100

    Finished first version of education cards

--- myown/src/components/myeducation/educationCard.js
+++ myown/src/components/myeducation/educationCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './educationCard.css';
 
-const EducationCard = ({ schoolName, logoSrc, description, years }) => {
+const EducationCard = ({ schoolName, logoSrc, description, years, program_description }) => {
   return (
     <div className="education-card">
       <div className="card-header">
@@ -12,7 +12,7 @@ const EducationCard = ({ schoolName, logoSrc, description, years }) => {
         </div>
       </div>
       <div className='card-content'>
-        
+        <div dangerouslySetInnerHTML={{ __html: program_description }} />
       </div>
       <div className='years'>
         {years}
--- myown/src/components/myeducation/myEducation.js
+++ myown/src/components/myeducation/myEducation.js
@@ -12,18 +12,21 @@ const MyEducation = () => {
           schoolName={'Lyceum BSU'} 
           description={'High school with majoring in math and physics'}
           years={'2015-2017'}
+          program_description={'A specialized lyceum with in-depth study of mathematics and physics. Participant in national Olympiads in these subjects.'}
           ></EducationCard>
           <EducationCard 
           logoSrc={'/education_icons/uppsala_icon.png'}
           schoolName={'Uppsala University'}
           description={'Game-design and programming'}
           years={'2019-2020'}
+          program_description={'During my education at Uppsala University, I had game design as my major and programming as my minor. <br/><br/> Mainly, the education was aimed at group projects, how to deal with SCRUM methodology, and being a valuable member of development process. <br/><br/> Considering my minor, I was primarily focused on Unity engine and creating games with tiny pre-made C++ engines'}
           ></EducationCard>
           <EducationCard 
           logoSrc={'/education_icons/lnu_icon.png'}
           schoolName={'Linnaeus University'}
           description={'Computer science and software development'}
           years={'2020-2023'}
+          program_description={'For my education at LNU, I personally tried a huge range of different technologies and topics related to the computer science field. <br/><br/> Beginning with computer security and networks and ending with advanced mathematics. Low-level microchip programming with assembly and Pico SDK, database technology and web-development. <br/><br/> Eventually, I graduated with a computer science bachelor '}
           ></EducationCard>
         </div>
     </div>
