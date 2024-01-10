a839c45ffc161e253f3fcb7a42082527bee8a76d
WhiteW1ine <foxratmir2000@gmail.com>
2024-01-10 23:48:28 +0100

    Almost done with skills column

--- myown/src/components/aboutme/columns/SkillsColumns.js
+++ myown/src/components/aboutme/columns/SkillsColumns.js
@@ -3,7 +3,17 @@ import React from 'react';
 const SkillsColumn = () => {
   return (
     <div className="skills column">
-      
+      <div className='form'>
+        <p>programming languages and frameworks</p>
+        <p>Java(Spring, JavaFX)</p>
+        <p>JS(Vue, React, Node)</p>
+        <p>C#(.NET, Unity)</p>
+        <p>Testing(JUnit, Mockito)</p>
+        <p>SCRUM(Jira, Trello)</p>
+        <p>Database(MySQL, NoSQL)</p>
+        <p>Docker, Gradle</p>
+        <p>Git(GitHub, GitLab, Kraken)</p>
+      </div>
     </div>
   );
 };
